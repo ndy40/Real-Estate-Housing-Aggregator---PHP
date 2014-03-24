c093704d203c4f5311115d88941dbe1de82df5a8
Richard Bray <richardobray@yahoo.co.uk>
2014-03-24 21:39:51 +0000

    changes to dashboard

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -6,45 +6,112 @@
 		
 	</div>
 	<main role="main">
+		<section class="dash-bg c-white">
+			<div class="container fw-b fz-2">
+				IG9
+			</div>
+		</section>
+
 		<div class="page-break tt-u fw-b c-white">New Properties</div>
 
 		<section>
-			<div class="col-md-9">
-				test
-			</div>
-			<div class="col-md-3">
-				+28 <br> New properties this week
+			<div class="container">
+				<span class="fl-l mt-1">New properties this week</span>
+				<h1 class="ta-c c-green m-0 mb-0_5 fl-r">+28</h1>
 			</div>
 		</section>
 
 		<div class="page-break tt-u fw-b c-white">Average Prices</div>
 
-		<section class="mt-0_5">
-			<h1 class="ta-c c-green m-0">£1104.00 pcm</h1>
+		<section class="mt-0_5 mb-1">
+			<h1 class="ta-c c-green m-0 mb-0_5">£1104.00 pcm</h1>
 
-			<div class="btn-group" data-toggle="buttons">
-				<label class="btn btn-default">
-					<input type="radio" name="ros" id="rent"> Rent
-				</label>
-				<label class="btn btn-default">
-					<input type="radio" name="ros" id="sale"> Sale
-				</label>
-			</div>	
+			<section class="container">
+				<div class="btn-group fl-l r-gap" data-toggle="buttons">
+					<label class="btn btn-tap active">
+						<input type="radio" name="ros" id="rent"> Rent
+					</label>
+					<label class="btn btn-tap">
+						<input type="radio" name="ros" id="sale"> Sale
+					</label>
+				</div>	
 
-			<div class="dropdown">
-			<button class="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
-			Dropdown
-			<span class="caret"></span>
-			</button>
-				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-				<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-				<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-				<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-				<li role="presentation" class="divider"></li>
-				<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-				</ul>
-			</div>			
+				<div class="dropdown fl-l r-gap">
+					<button class="btn btn-tap dropdown-toggle sr-only" type="button" data-toggle="dropdown">
+						Detatched
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+						<li role="presentation"><a role="menuitem"  href="#">Option1</a></li>
+						<li role="presentation"><a role="menuitem"  href="#">Option2</a></li>
+					</ul>
+				</div>			
 
+				<div class="dropdown fl-l">
+					<button class="btn btn-tap dropdown-toggle sr-only" type="button" data-toggle="dropdown">
+						1 Bed
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+						<li role="presentation"><a role="menuitem"  href="#">2 Bed</a></li>
+						<li role="presentation"><a role="menuitem"  href="#">3 Bed</a></li>
+						<li role="presentation"><a role="menuitem"  href="#">4 Bed</a></li>
+					</ul>
+				</div>
+
+				<div class="cl-l"></div>
+
+			</section>
+		</section>
+
+		<div class="page-break tt-u fw-b c-white ">Market Shakers</div>
+
+		<section class="container block">
+			<article class="mt-1">
+				<div class="fl-l fw-b w-7">
+					<div class="c-grey sml-font">Asking Price increasaed</div>
+					<span>23 High Road, Buckhurst Hill, IG9 6</span>
+				</div>
+				<h1 class="ta-c c-green m-0 mb-0_5 fl-r">+4.7%</h1>
+			</article>
+		</section>
+
+		<section class="container block">
+			<article class="mt-1">
+				<div class="fl-l fw-b w-7">
+					<div class="c-grey sml-font">Asking Price increasaed</div>
+					<span>Flat 8, 23 Beatrice Court, Buckhurst Hill, IG9 6</span>
+				</div>
+				<h1 class="ta-c c-green m-0 mb-0_5 fl-r">+4.7%</h1>
+			</article>
+		</section>		
+
+		<div class="page-break tt-u fw-b c-white ">Latest Changes</div>
+
+		<section class="container block">
+			<article class="mt-1">
+				<div class="fw-b">
+					<div class="c-grey sml-font fl-l">Asking Price increasaed</div>
+					<div class="c-grey sml-font fl-r">Today 2:12PM</div>
+				</div>
+				<div class="cl fw-b">
+					<div class="fl-l" style="width:90%;">Flat 8, 23 Beatrice Court, Buckhurst Hill, IG9 6</div>
+					<i class="ion-ios7-arrow-right fz-2 fl-r c-grey mt-0_5"></i>
+				</div>
+			</article>
+		</section>
+
+		<section class="container block">
+			<article class="mt-1">
+				<div class="fw-b">
+					<div class="c-grey sml-font fl-l">Asking Price increasaed</div>
+					<div class="c-grey sml-font fl-r">Today 2:12PM</div>
+				</div>
+				<div class="cl fw-b">
+					<div class="fl-l" style="width:90%;">Flat 8, 23 Beatrice Court, Buckhurst Hill, IG9 6</div>
+					<i class="ion-ios7-arrow-right fz-2 fl-r c-grey mt-0_5"></i>
+				</div>
+			</article>
 		</section>
 
 	</main>
