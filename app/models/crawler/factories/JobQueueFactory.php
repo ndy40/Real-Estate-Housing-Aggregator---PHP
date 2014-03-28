17171192b1e7070a0b29ef2b3e9c3777032d8b2e
Ndifreke Ekott <ndy40.ekott@gmail.com>
2014-03-28 23:30:14 +0000

    Adding some pheadstalkd library and other dependencies

---
+++ app/models/crawler/factories/JobQueueFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace models\crawler\factories;
+
+/**
+ * Description of JobQueueFactory
+ *
+ * @author ndy40
+ */
+class JobQueueFactory {
+    //put your code here
+}
