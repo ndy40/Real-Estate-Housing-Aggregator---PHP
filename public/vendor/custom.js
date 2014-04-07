0824589986c43e7d1fce67c80c1ce10e62f17bf8
Richard Bray <richardobray@yahoo.co.uk>
2014-04-07 18:31:38 +0100

    adding more info to the top of the dashboard

--- public/vendor/custom.js
+++ public/vendor/custom.js
@@ -1,7 +1,10 @@
 
-// Bootstrap Stuff
+// Fastclick
 // --------------------------------------------------
 
+$(function() {
+    FastClick.attach(document.body);
+});
 
 
 // Navigation Toggle
@@ -20,11 +23,11 @@ $('.left-nav a').click(function() {toggleNav();});
 // --------------------------------------------------
 
 
-var msg = "<div class='mt-5' style='padding:1em;'><h1 class='c-green'>Property Crunch and large screens weren't meant to be! :( </h1> <br>Please view this on a mobile phone or resize your browser and <b>refresh</b> the page.</div>";
-
-if ($(window).width() >= 640){$('body').html(msg);}
-
-$(window).resize(function(){
-	if ($(window).width() >= 640){$('body').html(msg);}
-});
+//var msg = "<div class='mt-5' style='padding:1em;'><h1 class='c-green'>Property Crunch and large screens weren't meant to be! :( </h1> <br>Please view this on a mobile phone or resize your browser and <b>refresh</b> the page.</div>";
+//
+//if ($(window).width() >= 640){$('body').html(msg);}
+//
+//$(window).resize(function(){
+//	if ($(window).width() >= 640){$('body').html(msg);}
+//});
 
