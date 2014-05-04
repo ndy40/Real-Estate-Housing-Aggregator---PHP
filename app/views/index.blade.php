8739553878daa3d9cea72e4ab7d8342a7a780633
Richard Bray <richardobray@yahoo.co.uk>
2014-05-04 22:15:07 +0100

    added heap analytics

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -50,6 +50,12 @@
 
 		<script src="{{asset('vendor/fastclick.js')}}"></script>
 		<script src="{{asset('vendor/jquery.blockUI.js')}}"></script>
-		<script src="{{asset('vendor/custom.js')}}"></script>			
+		<script src="{{asset('vendor/custom.js')}}"></script>
+
+    <script type="text/javascript">
+      var heap=heap||[];heap.load=function(a){window._heapid=a;var b=document.createElement("script");b.type="text/javascript",b.async=!0,b.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.heapanalytics.com/js/heap.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c);var d=function(a){return function(){heap.push([a].concat(Array.prototype.slice.call(arguments,0)))}},e=["identify","track"];for(var f=0;f<e.length;f++)heap[e[f]]=d(e[f])};
+      heap.load("1998172223");
+    </script>
+					
 	</body>
 </html>
